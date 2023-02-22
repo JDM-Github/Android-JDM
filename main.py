0e2cc67e9d3b3eaaf0db3411e019b2c21cc84e39
JDMaster <numberonejdmaster@gmail.com>
2023-02-22 17:42:56 +0800

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from jdm_kivy import *
 from src import MainScreen, MainField
 
-class MainApp(JDMApp, App):
-    def __init__(self, title: str = None, size: list = ..., manager: JDMRootManager = None, **kwargs):
-        super().__init__(title, size, manager, **kwargs)
-
 if __name__ == "__main__":
-    MainApp("Calculator", (720*0.5, 1400*0.5)).run(screen=MainScreen(), widget=MainField())
+    JDMApp("Calculator", (720*0.5, 1400*0.5)).run(screen=MainScreen(), widget=MainField())
