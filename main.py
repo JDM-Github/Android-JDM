91e498f7960bd33d40abe87471d54fae2121cf01
JDMaster <numberonejdmaster@gmail.com>
2023-02-22 12:35:42 +0800

    Changes to be committed: 	modified:   build-config.json 	modified:   buildozer.spec 	modified:   main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from jdm_kivy import *
-from src import MainScreen, MainField
+# from jdm_kivy import *
+# from src import MainScreen, MainField
 
-if __name__ == "__main__":
-    JDMApp("Calculator", (720*0.5, 1400*0.5)).run(screen=MainScreen(), widget=MainField())
+# if __name__ == "__main__":
+#     JDMApp("Calculator", (720*0.5, 1400*0.5)).run(screen=MainScreen(), widget=MainField())
+
+from kivy.app import runTouchApp
+
+runTouchApp()
