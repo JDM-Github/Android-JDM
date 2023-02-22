a5d789a8f9cdf3943d7f9c4b81a0c1fd04d6a9d6
JDMaster <numberonejdmaster@gmail.com>
2023-02-22 17:35:02 +0800

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ from jdm_kivy import *
 from src import MainScreen, MainField
 
 if __name__ == "__main__":
-    JDMConfig.set("display_kivy_logs", False)
     JDMApp("Calculator", (720*0.5, 1400*0.5)).run(screen=MainScreen(), widget=MainField())
