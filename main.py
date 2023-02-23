581bf023ff2ef14813a34cbc0b85d3eb61cc43b3
JDMaster <numberonejdmaster@gmail.com>
2023-02-23 11:11:30 +0800

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from jdm_kivy import *
-from src import MainField
-from kivy.app import App
+from kivy.app import App, platform
+from kivy.core.window import Window
 from kivy.core.text import LabelBase
+from kivy.uix.label import Label
 
 LabelBase.register(
     name="consolas",
@@ -10,40 +10,29 @@ LabelBase.register(
     fn_italic="assets/font/consolas/consolas_italic.ttf",
     fn_bolditalic="assets/font/consolas/consolas_italic_bold.ttf")
 
+class JDMLabel(Label):
+    
+    def __init__(self, **kwargs):
+        self.font_name = "consolas"
+        self.bind(size=self.setter('text_size'))
+        self.valign = 'center'
+        self.halign = 'center'
+        super().__init__(**kwargs)
+
 class JDMApp(App):
 
-    def __init__(self, title: str = None, size: list = (500, 500), manager: JDMRootManager = None, **kwargs):
+    def __init__(self, title: str = None, size: list = (500, 500), **kwargs):
         super().__init__(**kwargs)
         if not platform == 'android':
             Window.size = size
             Window.left = 1
             Window.top = 30
-        self.root: JDMRootManager = manager if manager else JDMRootManager()
         self.title = title if title else __class__.__name__.removesuffix('App')
         self._main_title = self.title
 
-    def on_start(self):
-        if platform != 'android':
-            Window.bind(on_key_down=self.root.keyboard_down)
-            Window.bind(on_key_up=self.root.keyboard_up)
-            Window.bind(on_mouse_down=self.root.mouse_down)
-            Window.bind(on_mouse_move=self.root.mouse_move)
-            Window.bind(on_mouse_up=self.root.mouse_up)
-            Window.bind(mouse_pos=self.root._mouse_pos)
-        return super().on_start()
-
-    def run(self, screen_name: str = "main", screen: JDMScreen = None, widget: JDMWidget = None):
-        self.__first_screen = screen
-        self.__first_screen_name = screen_name
-        self.__first_widget = widget
-        return super().run()
-
     def build(self):
-        self.root.add_screen(
-            self.__first_screen_name,
-            self.__first_screen,
-            self.__first_widget)
+        self.root = JDMLabel(text="TEST")
         return self.root
 
 if __name__ == "__main__":
-    JDMApp("Calculator", (720*0.5, 1400*0.5)).run(widget=MainField())
+    JDMApp("Calculator", (720*0.5, 1400*0.5)).run()
