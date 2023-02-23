bcbf6d549831517ca7dd00f97b305b6b74253dc5
JDMaster <numberonejdmaster@gmail.com>
2023-02-23 14:52:31 +0800

    Changes to be committed: 	new file:   .github/workflows/build-calculator.yml 	modified:   main.py

--- main.py
+++ main.py
@@ -11,4 +11,3 @@ from src import MainField, MainScreen, JDMApp
 
 if __name__ == "__main__":
     JDMApp("Calculator", (720*0.5, 1400*0.5)).run(screen_name="main", screen=MainScreen(), widget=MainField())
-
