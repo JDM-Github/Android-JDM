28ac615a09f5bee9b4afe6c20892032f1832294e
JDMaster <numberonejdmaster@gmail.com>
2023-02-23 13:20:32 +0800

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -11,3 +11,4 @@ from src import MainField, MainScreen, JDMApp
 
 if __name__ == "__main__":
     JDMApp("Calculator", (720*0.5, 1400*0.5)).run(screen_name="main", screen=MainScreen(), widget=MainField())
+
